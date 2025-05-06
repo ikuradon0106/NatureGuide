4c825d4f14670f62ecb3ba553cc5ce2e4a21dd77
EC2 Default User <ec2-user@ip-172-31-40-77.ap-northeast-1.compute.internal>
2025-05-06 00:47:22 +0000

    [Next] create migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :group_users
+  devise_for :groups
+  devise_for :searches
+  devise_for :comments
+  devise_for :categories
+  devise_for :posts
   devise_for :admins
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
