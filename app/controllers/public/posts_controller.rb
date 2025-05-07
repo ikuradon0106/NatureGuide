35c528279b6f7140566749ececaaa1830d64ce86
EC2 Default User <ec2-user@ip-172-31-40-77.ap-northeast-1.compute.internal>
2025-05-07 10:26:40 +0000

    [finish]Post_new

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -2,24 +2,43 @@ class Public::PostsController < ApplicationController
   # 該当ユーザーとしてログインしている場合のみアクセス許可
   before_action :authenticate_user!
 
+  # 新規投稿の表示
   def new
+    @post = Post.new
   end
 
+  # 投稿一覧の表示
   def index
   end
 
+  # 投稿詳細の表示
   def show
   end
 
+  # 投稿データの保存
   def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.save
+    redirect_to new_post_path
   end
 
+  # 投稿の編集画面
   def edit
   end
 
+  # 投稿の更新処理
   def update
   end
 
+  # 投稿の削除処理
   def destroy
+  end
+
+  # 投稿データのストロングパラメータ
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :latitude, :longitude, :image)
   end
 end
