0b8eb8cf9ec142b93d16adc4f0aa2eb79014bfb2
EC2 Default User <ec2-user@ip-172-31-40-77.ap-northeast-1.compute.internal>
2025-05-16 00:23:31 +0000

    finish

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -12,12 +12,27 @@ class Admin::UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  # 編集画面の表示
   def edit
     @user = User.find(params[:id])
   end
 
+  # 編集更新処理
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "編集に成功しました。"
+      redirect_to admin_user_path(@user)
+    else
+      flash.now[:alert] = "編集に失敗しました。"
+      render :edit
+    end
   end
 
+  # ユーザーデータのストロングパラメータ
+  private
 
+  def user_params
+    params.require(:user).permit(:email, :nickname, :user_image, :introduction, :is_active)
+  end
 end
