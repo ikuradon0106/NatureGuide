281356d0d981cb3a1747ca5e3e79f4381ae84e61
EC2 Default User <ec2-user@ip-172-31-40-77.ap-northeast-1.compute.internal>
2025-05-25 03:11:04 +0000

    fixes-controller

--- app/controllers/public/group_requests_controller.rb
+++ app/controllers/public/group_requests_controller.rb
@@ -1,13 +1,13 @@
 class Public::GroupRequestsController < ApplicationController
-  # 該当ユーザーとしてログインしている場合のみアクセス許可
+  # ユーザーがログインしている場合のみ、各アクションを実行する（未ログイン時はリダイレクト）
   before_action :authenticate_user!
 
-  # グループ申請処理
+  # グループ参加申請処理
   def create
     @group = Group.find(params[:group_id])
 
     # 申請データを生成（どのグループに誰が申請するかを指定）
-    # この記述で、group_id = @group.id, user_id = current_user.id とRailsが内部で自動的に変換
+    # group_id = @group.id, user_id = current_user.id とRailsが内部で自動的に変換する
     @group_request = GroupRequest.new(
       group: @group,
       user: current_user
@@ -16,38 +16,50 @@ class Public::GroupRequestsController < ApplicationController
     if @group_request.save
       flash[:notice] = "グループへの参加申請を送信しました"
     else
-      flash[:alert] = "申請に失敗しました"
+      flash[:alert] = "グループへの参加申請に失敗しました"
     end
+
     redirect_to group_path(@group)
   end
 
-  # グループ申請の承認・拒否処理
+  # グループ参加申請の承認・拒否処理
   def update
     @group_request = GroupRequest.find(params[:id])
     @group = @group_request.group
 
+    # グループのオーナーの承認・拒否
     if current_user.id == @group.owner_id
+
+      # 承認か拒否かの判断は、params[:decision] の値によって分岐
       if params[:decision] == "approve"
+         # ステータスがapproveならば、「承認済み」に更新
         @group_request.update(status: "approved")
         flash[:notice] = "#{@group_request.user.nickname}さんの申請を承認しました"
+
+        
       elsif params[:decision] == "reject"
+        # ステータスがrejectならば、「拒否」に更新
         @group_request.update(status: "rejected")
         flash[:alert] = "#{@group_request.user.nickname}さんの申請を拒否しました"
       else
+        # 想定外のパラメータが渡された場合
         flash[:alert] = "不正な操作です"
       end
+
     else
+      # グループオーナー以外が承認・拒否しようとした場合
       flash[:alert] = "権限がありません"
     end
+
     redirect_to group_path(@group)
   end
 
-  # 申請したユーザーのグループ申請を削除処理
+  # 申請したユーザーのグループ申請を削除する処理
   def destroy
     @group_request = GroupRequest.find(params[:id])
     @group = @group_request.group
 
-    # 申請者本人かどうかチェックし、削除
+    # 申請者本人かどうかチェックし、申請を削除
     if @group_request.user_id == current_user.id
       @group_request.destroy
       flash[:notice] = "参加申請をキャンセルしました"
@@ -56,4 +68,5 @@ class Public::GroupRequestsController < ApplicationController
     end
     redirect_to group_path(@group)
   end
+
 end
