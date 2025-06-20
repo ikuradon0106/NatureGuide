4c745518bd5d928626ab5d64ea6ccdf2ba097d68
EC2 Default User <ec2-user@ip-172-31-40-77.ap-northeast-1.compute.internal>
2025-06-20 22:44:31 +0000

    seedの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+puts "seedの実行を開始"
+
+# find_or_create_by!メソッドで、既存のデータがある場合はそのデータが取得されるのみで、登録処理が実行されないようにする
 # 管理者用パスワード
-Admin.create!(
-  email: "admin@example.com",
-  password: "password",
-  password_confirmation: "password"
-)
+Admin.find_or_create_by!(email: "admin@example.com") do |admin|
+  adimn.password = "password"
+  admin.password_confirmation = "password"
+end
+
+
+# テストユーザー（一般ユーザー）アカウント
+User.find_or_create_by!(email: "testuser@example.com") do |user|
+  user.nickname = "テストユーザー"
+  user.password = "natureguide"
+  user.password_confirmation = "natureguide"
+end
+
+puts "seedの実行が終了しました"
